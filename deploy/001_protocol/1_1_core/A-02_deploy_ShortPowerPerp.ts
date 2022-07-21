e86a79a492ab8db738ff9a71f1577069910a93da
Ratimon <ratixoxo@gmail.com>
2022-07-21 14:15:18 +0700

    Feat & Build (./src & ./deploy): Adding ShortPowerPerp contract and its deploy scripts

---
+++ deploy/001_protocol/1_1_core/A-02_deploy_ShortPowerPerp.ts
@@ -0,0 +1,84 @@
+import 'dotenv/config';
+
+import chalk from 'chalk';
+
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import {DeployFunction} from 'hardhat-deploy/types';
+
+import {utils,} from 'ethers';
+
+const { formatUnits,parseEther,parseUnits} = utils;
+
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+    
+  const {deployments, getNamedAccounts, network} = hre;
+  const {deploy, execute , log } = deployments;
+  const {deployer} = await getNamedAccounts();
+
+  log(chalk.cyan(`.....`));
+  log(chalk.cyan(`Starting Script.....`));
+
+  log(`Deploying contracts with the account: ${deployer}`);
+
+
+  const balance = await hre.ethers.provider.getBalance(deployer);
+  log(`Account balance: ${formatUnits(balance, 'ether')} ETH`);
+
+
+  log(chalk.yellow(`Network Name: ${network.name}`));
+  log("----------------------------------------------------")
+
+
+
+  const  Args : {[key: string]: any} = {};
+
+  Args[`name`] = 'short Squeeth'
+  Args[`symbol`] = 'sSQTH'
+
+  const deploymentName = "ShortPowerPerp"
+  const Result = await deploy(deploymentName,{
+    contract: "ShortPowerPerp",
+    from: deployer,
+    args: Object.values(Args),
+    log: true,
+    skipIfAlreadyDeployed: true}
+  );
+  
+  log("------------------ii---------ii---------------------")
+  log(`Could be found at ....`)
+  log(chalk.yellow(`/deployment/${network.name}/${deploymentName}.json`))
+
+  if (Result.newlyDeployed) {
+    
+    log(`contract address: ${chalk.green(Result.address)} using ${Result.receipt?.gasUsed} gas`);
+
+    for(var i in Args){
+      log(chalk.yellow( `Argument: ${i} - value: ${Args[i]}`));
+    }    
+
+    if( network.name != 'hardhat' && ( hre.network.tags.production || hre.network.tags.staging)){
+
+      try {
+          
+          await hre.run("verify:verify", {
+              address: Result.address,
+              constructorArguments: Object.values(Args),
+          });
+
+          }
+      catch(err) {
+          console.log(err)
+      }
+    }
+    
+  }
+
+  log(chalk.cyan(`Ending Script.....`));
+  log(chalk.cyan(`.....`)); 
+
+    
+}
+export default func;
+func.tags = ["11-A2","11","short-power-perp","core","protocol"];
+func.dependencies = ["11-A1"];
